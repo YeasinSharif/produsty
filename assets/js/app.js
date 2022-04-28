fa6de677eceb76ab68374c9e5cd1618d16024b07
YeasinSharif <mahfujakash2@gmail.com>
2022-04-28 16:33:32 +0600

    c

--- assets/js/app.js
+++ assets/js/app.js
@@ -210,7 +210,32 @@ $(function () {
     centerPadding: '0px',
     prevArrow: $('.our-arrow-l'),
     nextArrow: $('.our-arrow-r'),
-    speed: 300
+    speed: 300,
+    responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                   
+                }
+    },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+    },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+  ]
 
   });
   /* our team slider */
